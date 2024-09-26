862bed481398c258da2a11deb027911802dfee1a
Aleksandr_Miziyk <muzikbasss@gmail.com>
2024-09-26 15:34:33 +0200

    feat: modified:   package-lock.json         modified:   package.json         modified:   src/Componets/Contacts/Contacts.jsx

--- src/Componets/Contacts/Contacts.jsx
+++ src/Componets/Contacts/Contacts.jsx
@@ -163,7 +163,7 @@ const Contacts = ({ privacyPolicy, setPrivacyPolicy, getAllTextsOfSite }) => {
 								</a>
 							</div>
 							<div className="contacts__social ">
-								<a href='https://www.linkedin.com/in/%D0%B0%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80-%D0%BC%D0%B8%D0%B7%D1%8E%D0%BA-290766112/' className="contacts__links">
+								<a href='https://www.linkedin.com/in/oleksandr-miziuk-7297772a4' className="contacts__links">
 									<img className='linkedin' src={Linkedin} alt={'Linkedin'} />
 								</a>
 							</div>
